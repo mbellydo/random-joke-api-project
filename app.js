22aef19d2f60446ba1826eeb311b2e0f37104f0f
marc <mbellydo@gmail.com>
2020-12-29 14:26:40 +0100

    Done

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // Tu códgigo aquí
+const image = document.querySelector('img');
+const jokeDIV = document.querySelector('#display-joke');
+const button = document.querySelector('#get-joke');
+
+button.addEventListener('click', function(){
+    getRandomJoke();
+})
+
+function getRandomJoke(){
+    const ajax = new XMLHttpRequest;
+
+    ajax.open('GET', 'https://api.chucknorris.io/jokes/random', true);
+
+    ajax.onreadystatechange = function(){
+        if(this.status === 200 && this.readyState === 4){
+            console.log(this.responseText);
+            let data = JSON.parse(this.responseText);
+            jokeDIV.innerHTML = `${data.value}`
+        } else {
+            this.onerror = onerror();
+        }
+    }
+    ajax.send();
+}
+
+function onerror(){
+    jokeDIV.textContent = 'Something has gone wrong!';  
+}
